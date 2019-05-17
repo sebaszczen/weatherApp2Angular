c07af87bf83788bb602c570b7a10b7211e4733e6
sebaszczen <sebastianszczebiot@gmail.com>
2019-05-17 09:33:32 +0200

    login works

--- src/app/services/data/user/user-data.service.ts
+++ src/app/services/data/user/user-data.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { UserDto } from "../../../../../../weatherApp/src/app/models/userDto";
-import { Observable } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserDataService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private routerModule: Router, private httpClient: HttpClient) {}
 
   handleLogin(user: UserDto) {
     let headers = new HttpHeaders({
@@ -19,14 +19,18 @@ export class UserDataService {
     let options = { headers: headers};
 
     let params = new HttpParams()
-    .append("username", "ad@gmail.com")
+    .append("username", "a")
     .append("password", "a")
     .append("rolename", "Admin");
 
     this.httpClient
       .post("//localhost:8080/login", { username: "ad@gmail.com",
-      password: "a"}, {headers, params})
-      .subscribe(error => console.log('There was an error: '));
+      password: "a"}, {headers, params, observe: 'response'})
+      .subscribe(response=> {
+        if(response.status==200){
+          this.routerModule.navigate(['http://localhost:4200']);
+        }
+      }, error=> {console.log(error)});
 
 
   }
